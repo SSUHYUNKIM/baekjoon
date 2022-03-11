dd7ea255ea487c73ef96607f005404d24152448c
KIMSUSUSUH <yellosuhyun@naver.com>
2022-03-12 01:29:24 +0900

    #10871

---
+++ B3/10871/java/j10871.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class j10871 {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int x = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+      int num = sc.nextInt();
+      if (num < x) {
+        System.out.print(num + " ");
+      }
+    }
+  }
+}
