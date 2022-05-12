afb54b878dcda780515658346a0512de8e6e3178
KIMSUSUSUH <yellosuhyun@naver.com>
2022-05-12 17:06:38 +0900

    #1000

---
+++ B5/1000/c#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(input[0]) + int.Parse(s[1]));
+        }
+    }
+}
