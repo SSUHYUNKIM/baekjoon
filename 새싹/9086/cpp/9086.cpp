c2f22cb859c31590b0764b11f437e1e8ba96649c
KIMSUSUSUH <yellosuhyun@naver.com>
2022-06-29 16:31:53 +0900

    #9086

--- 새싹/9086/cpp/9086.cpp
+++ 새싹/9086/cpp/9086.cpp
@@ -8,11 +8,7 @@ int main(){
   string input;
   for(i = 0; i < t; i++){
     cin >> input;
-    if(input.length() == 1){
-      cout << input[0] << input[0] << endl;
-    } else{
-      cout << input[0] << input[input.length() - 1] << endl;
-    }
+    cout << input[0] << input[input.length() - 1] << endl;
   }
   return 0;
 }
