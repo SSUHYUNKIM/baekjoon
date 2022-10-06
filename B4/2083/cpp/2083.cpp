33e8f4cdd9f610c5afc1b00993ebf460515eaf93
SSHYUNK <shyunk@fgb.ai>
2022-10-06 16:53:53 +0900

    #2083

---
+++ B4/2083/cpp/2083.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age, weight;
+    while (cin >> name >> age >> weight)
+    {
+        if (name == "#" && age == 0 && weight == 0)
+            break;
+
+        if (age > 17 || weight >= 80)
+            cout << name << " Senior" << endl;
+        else
+            cout << name << " Junior" << endl;
+    }
+    return 0;
+}
