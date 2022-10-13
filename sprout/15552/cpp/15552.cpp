b4653bb34936285f567af634bcaa7577243c630b
SSHYUNK <shyunk@fgb.ai>
2022-10-13 15:52:22 +0900

    #15552

---
+++ sprout/15552/cpp/15552.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t, a, b;
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+    return 0;
+}
