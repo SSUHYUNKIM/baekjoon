1601fdd18c85e8b988b860655aac8399375e10aa
SSHYUNK <shyunk@fgb.ai>
2022-10-14 10:32:10 +0900

    #2440

--- B4/2440/cpp/2440.cpp
+++ B4/2440/cpp/2440.cpp
@@ -6,7 +6,7 @@ int main(){
     int n;
     cin >> n;
     for(int i = n; i > 0; i--){
-        for(int j = 0 ; j < n; j++){
+        for(int j = 0 ; j < i; j++){
             cout << "*";
         }
         cout << "\n";
