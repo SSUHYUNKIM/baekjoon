46f34cd5228094237bbdf9ef504c6b78425af5f2
SSUHYUNKIM <yellosuhyun@naver.com>
2023-01-09 12:55:01 +0900

    #2752

---
+++ B4/2752/cpp/2752.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int num[3];
+    
+    for(int i = 0; i < 3; i++)
+    {
+        cin >> num[i];
+    }
+    sort(num, num + 3);
+    
+    for(int i = 0; i < 3; i++)
+    {
+        cout << num[i] << ' ';
+    }
+    return 0;
+}
