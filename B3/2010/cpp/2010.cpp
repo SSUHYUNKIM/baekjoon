99708c249fda1f6a981c895d165e94d07d859ccb
SSUHYUNKIM <yellosuhyun@naver.com>
2023-01-10 11:35:46 +0900

    #2441

--- B3/2010/cpp/2010.cpp
+++ B3/2010/cpp/2010.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main()
 {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
     int n, plug, result = 0;
     cin >> n;
 
@@ -11,7 +13,7 @@ int main()
         cin >> plug;
         result += plug;
     }
-    
+
     cout << result - n + 1 << endl;
 
     return 0;
