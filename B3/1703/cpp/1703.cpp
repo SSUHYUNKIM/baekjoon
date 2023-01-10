7d1cd81832b1e1d7900bc608c687844b8712c782
SSUHYUNKIM <yellosuhyun@naver.com>
2023-01-10 11:22:47 +0900

    #1703

---
+++ B3/1703/cpp/1703.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    long long a, sf, pr, result;
+
+    while (true)
+    {
+        result = 1;
+        cin >> a;
+        if (a == 0)
+            break;
+        for (int i = 0; i < a; i++)
+        {
+            cin >> sf >> pr;
+            result *= sf;
+            result -= pr;
+        }
+        cout << result << "\n";
+    }
+    
+    return 0;
+}
