1982b0285a4a3f7bd910d1cef8fa97f2382c6d21
SSUHYUNKIM <yellosuhyun@naver.com>
2023-01-19 21:58:33 +0900

    #1037

---
+++ B1/1037/cpp/1037.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int arr[51] = {};
+    int n;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+    cout << arr[0] * arr[n - 1];
+}
