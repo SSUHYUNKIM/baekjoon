7cc0b5852314c436b7afd4da6a7cbb11738b1a54
SSUHYUNKIM <yellosuhyun@naver.com>
2023-01-19 21:30:13 +0900

    #2869

---
+++ B1/2869/cpp/2869.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int a, b, v, height = 0, result = 1;
+
+    cin >> a >> b >> v;
+
+    result += (v-a) / (a-b);
+    if((v - a) % (a - b) != 0)
+        result++;
+
+    cout << result <<endl;
+
+    return 0;
+}
