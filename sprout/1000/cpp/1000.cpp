f5dbb1cbfa290e6822b4fb6e90bc81bcd43ce728
SSUHYUNKIM <yellosuhyun@naver.com>
2023-02-28 15:40:04 +0900

    #10988

--- sprout/1000/cpp/1000.cpp
+++ sprout/1000/cpp/1000.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-  int a, b;
-  
-  cin >> a >> b;
-  
-  cout << a + b;
-  return 0;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + b;
+    return 0;
 }
