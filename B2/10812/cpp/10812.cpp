705091f32e8c5c25e09fbcde6f4ae8d876b163d6
SSUHYUNKIM <yellosuhyun@naver.com>
2023-02-28 15:26:48 +0900

    #10812

--- B2/10812/cpp/10812.cpp
+++ B2/10812/cpp/10812.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    
+    int n, m, i, j, k;
+    vector<int> basket(101);
+
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; i++)
+    {
+        basket[i] = i;
+    }
+
+    for(int o = 1; o <= m; o++)
+    {
+        cin >> i >> j >> k;
+        rotate(basket.begin() + i, basket.begin() + k, basket.begin() + j + 1);
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        cout << basket[i] << " ";
+    }
+
     return 0;
 }
--- sprout/2557/cpp/2557.cpp
+++ sprout/2557/cpp/2557.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main()
 {
-  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-  cout << "Hello World!" << endl;
-  return 0;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    cout << "Hello World!";
+    return 0;
 }
