00e0cf9653152f484743c3aaf37a3e858bc3fb15
SSUHYUNKIM <yellosuhyun@naver.com>
2023-03-04 00:16:28 +0900

    #2581

--- sprout/9498/cpp/9498.cpp
+++ sprout/9498/cpp/9498.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 int main()
 {
-  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-  int score;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int score;
 
-  cin >> score;
+    cin >> score;
 
-  if (score >= 90)
-    cout << "A";
-  else if (score >= 80 && score < 90)
-    cout << "B";
-  else if (score >= 70 && score < 80)
-    cout << "C";
-  else if (score >= 60 && score < 70)
-    cout << "D";
-  else
-    cout << "F";
-
-  return 0;
+    if (score >= 90)
+        cout << "A";
+    else if (score >= 80 && score < 90)
+        cout << "B";
+    else if (score >= 70 && score < 80)
+        cout << "C";
+    else if (score >= 60 && score < 70)
+        cout << "D";
+    else
+        cout << "F";
+    
+    return 0;
 }
