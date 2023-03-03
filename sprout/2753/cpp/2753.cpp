2fa36f97418ec36585a3385b9a8de38e5bda8212
SSUHYUNKIM <yellosuhyun@naver.com>
2023-03-04 00:24:26 +0900

    #11653

--- sprout/2753/cpp/2753.cpp
+++ sprout/2753/cpp/2753.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int main()
 {
-  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-  int input;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int input;
 
-  cin >> input;
+    cin >> input;
 
-  if(input % 4 == 0)
-  {
-    if(input % 100 == 0 && input % 400 != 0)
-      cout << "0";
+    if(input % 4 == 0)
+    {
+        if(input % 100 == 0 && input % 400 != 0)
+            cout << "0";
+         else
+            cout << "1";
+    } 
     else
-      cout << "1";
-  } 
-  else
-      cout << "0";
-      
-  return 0;
+        cout << "0";
+    
+    return 0;
 }
